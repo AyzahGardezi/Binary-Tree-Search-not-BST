cc16dc446e1a2e7346a47fb60c89c73801d5b46f
Ayzah Gardezi <116636136+AyzahGardezi@users.noreply.github.com>
2023-05-31 14:23:35 +0500

    Create SearchNonBST.cpp

---
+++ SearchNonBST.cpp
@@ -0,0 +1,15 @@
+struct Node{
+  int val;
+  Node *left = nullptr;
+  Node *right = nullptr;
+};
+
+bool search(int n, Node *root){
+  if (!root)
+    return false;
+  
+    if (root->val == n)
+      return true;
+    else
+      return search(n, root->left) || search(n, root->right);
+}
